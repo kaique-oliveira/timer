5e492632ad1dc559d341135ea84f4cf392faa953
Kaique Oliveira <kaiqueoliveiraantonio@gmail.com>
2023-06-28 22:19:08 -0300

    Implementado as novas rotas

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,14 +1,17 @@
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
-      <ThemeProvider theme={defaultTheme}>
-        <h1>timer</h1>
-
-        <GlobalStyle />
-      </ThemeProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
